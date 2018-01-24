880d437d513519848ff84d55c25ac29add0b386e
davidliu-desktop <davidliu@astri.org>
2018-01-24 12:13:15 +0800

    add trimBrackets return null when trimHKID invalid

--- src/main/java/org/astri/icdd/asic/CountryTool.java
+++ src/main/java/org/astri/icdd/asic/CountryTool.java
@@ -6,6 +6,8 @@ import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.annotation.Nullable;
+
 /**
  * Created by davidliu on 2/4/2017.
  */
@@ -25,21 +27,23 @@ public class CountryTool {
         return countries;
     }
     public static boolean isHKIDValid(String hkid) {
-        return isHKIDValid(hkid,false);
+        return trimHKID(hkid,false)!=null;
     }
-    public static boolean isHKIDValid(String hkid,boolean isStrict) {
-
+    public static String trimBrackets(String string){
+        return string.replaceAll("[()]","");
+    }
+    public static @Nullable String trimHKID(String hkid,boolean isStrict){
         String HKID = hkid.toUpperCase();
         int length = HKID.length();
         if(isStrict){
-            if (HKID.length() < 10||HKID.length()>11) return false;
+            if (HKID.length() < 10||HKID.length()>11) return null;
         }else {
-            if (length < 8||length>11) return false;
+            if (length < 8||length>11) return null;
         }
         if (HKID.charAt(length - 3) == '(' && HKID.charAt(length - 1) == ')') {
             HKID = HKID.substring(0, length - 3) + HKID.charAt(length - 2);
         }else {
-            if(isStrict)return  false;
+            if(isStrict)return  null;
         }
 
         String pattern = "^([A-Z{1,2}])([0-9]{6})([A0-9])$";
@@ -48,7 +52,7 @@ public class CountryTool {
         Matcher m = r.matcher(HKID);
 
 
-        if (!m.find()) return false;
+        if (!m.find()) return null;
         String prefix = m.group(1);
         String serial = m.group(2);
         String checkdigit = m.group(3);
@@ -65,14 +69,15 @@ public class CountryTool {
             long reminder = value % 11;
             long valid_checkdigit = 11 - reminder;
             if ("A".equalsIgnoreCase(checkdigit) && valid_checkdigit == 10) {
-                return true;
+                return HKID;
             }
             if (valid_checkdigit == Integer.parseInt(checkdigit)) {
-                return true;
+                return HKID;
             }
         } catch (NumberFormatException e) {
             e.printStackTrace();
         }
-        return false;
+        return null;
     }
+
 }
